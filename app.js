4dc39b488f591464bfd7ef2714438be73af741fc
Marc Pomar <marc@faable.com>
2017-07-14 10:14:51 +0200

    repostars step 1

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const debug = require('debug')(`repostars:${path.basename(__filename).split('.')[0]}`);
+
+const expressLayouts = require('express-ejs-layouts');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -13,9 +16,11 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout','layout/main');
+app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
